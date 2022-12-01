0bdce7fedbd36979d67d593e2a0361be28a1f579
Colin Currie <colin@myroadtowembley.com>
2022-12-01 15:52:03 +0000

    WIP: updated the styling on the articles page, added a header to the page layout
    Still got to add styling to the pagination (or replace with infinite scrolling)

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 
+  helper_method :pluralise_likes
+
   def index
     source_url = ENV.fetch('SOURCE_URL','http://s3-eu-west-1.amazonaws.com/olio-staging-images/developer/test-articles-v4.json')
     data = JSON.parse(HTTParty.get(source_url).body)
@@ -11,5 +13,13 @@ class ArticlesController < ApplicationController
     end
 
     @articles = Article.order(created_at: :desc).page( params[:page] )
+  end
+
+  def pluralise_likes( number )
+    case number
+      when 0 then 'No likes yet'
+      when 1 then '1 like'
+      else "#{number} likes"
+    end
   end
 end
